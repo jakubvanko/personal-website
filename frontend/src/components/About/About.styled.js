77bb3b1b0c2e32de494b108e9a76f88356d80e7a
jakubvanko <kubko.vanko@gmail.com>
2020-02-03 17:06:47 +0100

    Made about page responsive

--- frontend/src/components/About/About.styled.js
+++ frontend/src/components/About/About.styled.js
@@ -4,10 +4,19 @@ export const Container = styled.div`
     padding: 0 30px;
     max-width: 100vw;
     position: relative;
+    font-size: 14px;
     
     @media only screen {
+        @media (min-width: 400px) {
+            font-size: 15px;
+        }
+    
         @media (min-width: 992px) {
-            padding: 0 110px 0 80px;
+            font-size: 1.16vw;
+        }
+    
+        @media (min-width: 992px) {
+            padding: 50px 6.875vw 0 5vw;
             display: grid;
             grid-template-columns: auto auto auto;
             grid-template-areas: "heading text lists";
@@ -17,16 +26,18 @@ export const Container = styled.div`
             :before {
                 height: 100%;
                 background-color: #525F95;
-                width: 30px;
+                width: 1.875vw;
                 position: absolute;
                 top: 0;
                 left: 0;
                 content: "";
                 box-shadow: 3px 0 5px rgba(0,0,0,0.3);
             }
+            min-height: 100vh;
             
-            @media (orientation: landscape) {
-                min-height: 100vh;
+            @media (orientation: portrait) {
+                grid-template-columns: auto auto;
+                grid-template-areas: "heading text" "heading lists";
             }
         }
     }
@@ -41,13 +52,13 @@ export const Heading = styled.h2`
     
     @media only screen {
         @media (min-width: 992px) {
-            padding: 50px 40px 0 40px;
+            padding: 0 2.5vw;
             writing-mode: vertical-lr;
             text-orientation: upright;
             text-align: center;
-            font-size: 40px;
+            font-size: 5.05vh;
             text-transform: uppercase;
-            letter-spacing: 12px;
+            letter-spacing: 0.3em;
             font-weight: 700;
         }
     }
@@ -57,24 +68,22 @@ export const Text = styled.p`
     color: black;
     text-decoration: none;
     display: block;
-    font-size: 14px;
-    letter-spacing: 0.75px;
-    line-height: 30px;
+    letter-spacing: 0.042em;
+    line-height: 1.68em;
     
     @media only screen {
-        @media (min-width: 400px) {
-            font-size: 15px;
-        }
-    
         @media (min-width: 992px) {
             text-align: center;
-            font-size: 18px;
+            
+            @media (orientation: portrait) {
+                font-size: 1.8em;
+                align-self: flex-end;
+            }
         }
     }
 `;
 
 export const ListText = styled(Text)`
-
     @media only screen {
         @media (min-width: 992px) {
             text-align: left;
@@ -90,7 +99,11 @@ export const SubHeading = styled.h2`
     
     @media only screen {
         @media (min-width: 992px) {
-            font-size: 26px;
+            font-size: 1.445em;
+            
+            @media (orientation: portrait) {
+                font-size: 2.3em;
+            }
         }
     }
 `;
@@ -98,11 +111,19 @@ export const SubHeading = styled.h2`
 export const ListContainer = styled.div`
     @media only screen {
         @media (min-width: 992px) {
-            padding: 0 40px;
+            padding: 0 2.5vw;
             height: 75%;
             display: flex;
             flex-direction: column;
             justify-content: space-evenly;
+            
+            @media (orientation: portrait) {
+                justify-self: center;
+                justify-content: space-evenly;
+                align-self: flex-start;
+                padding-top: 4vh;
+                padding-bottom: 4vh;
+            }
         }
     }
 `;
