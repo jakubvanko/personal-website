ba1166b5277f915d5797386a1e7a983b1e7c6eb7
jakubvanko <kubko.vanko@gmail.com>
2020-02-07 19:03:23 +0100

    Moved form logic to react

--- frontend/src/scenes/Contact/components/ContactForm.js
+++ frontend/src/scenes/Contact/components/ContactForm.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
+
 import Button from "../../../components/Button/Button";
 
 const Form = styled.form`
     box-shadow: 0 3px 6px #00000066;
     width: 100%;
-    height: 50vh;
-    min-height: 20em;
+    min-height: max(50vh, 20em, 320px);
     position: relative;
     // Not sure about the margin here
     margin: 1vh 0;
@@ -29,11 +29,15 @@ const Form = styled.form`
 
 const TextArea = styled.textarea`
     font-size: max(16px, 1em);
-    color: #707070;
+    color: black;
     border: none;
     outline: none;
     padding: 1em;
     width: 100%;
+    
+    ::placeholder {
+        color: #707070;
+    }
     
     @media only screen {
         @media (min-width: 992px) {            
@@ -60,11 +64,23 @@ const FormButton = styled(Button)`
 
 
 const ContactForm = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [text, setText] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        // TODO: API CALL TO HANDLE SUBMIT
+        setName("");
+        setEmail("");
+        setText("Thank you for contacting me.\n\nI will get to you as soon as possible.\n\n~ Jakub Vanko");
+    };
+
     return (
-        <Form>
-            <Input placeholder={"NAME"}/>
-            <Input placeholder={"EMAIL"}/>
-            <TextArea placeholder={"MESSAGE"}/>
+        <Form onSubmit={handleSubmit}>
+            <Input placeholder={"NAME"} value={name} onChange={e => setName(e.target.value)}/>
+            <Input placeholder={"EMAIL"} value={email} onChange={e => setEmail(e.target.value)}/>
+            <TextArea placeholder={"MESSAGE"} value={text} onChange={e => setText(e.target.value)}/>
             <FormButton>Send message</FormButton>
         </Form>
     );
