636dc87dccec84220e6ff5d083a975dced407308
jakubvanko <kubko.vanko@gmail.com>
2023-05-14 16:59:41 +0200

    feat: remove footer

--- src/scenes/Main.js
+++ src/scenes/Main.js
@@ -7,7 +7,6 @@ import Home from "./Home/Home";
 import About from "./About/About";
 import Portfolio from "./Portfolio/Portfolio";
 import Contact from "./Contact/Contact";
-import Footer from "./Footer/Footer";
 
 const Main = () => (
     <>
@@ -16,7 +15,6 @@ const Main = () => (
         <About/>
         <Portfolio/>
         <Contact/>
-        <Footer/>
     </>
 );
 
